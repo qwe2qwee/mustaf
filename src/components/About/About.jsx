491ad3c61b0c91a6f28d1caf7f361ac1610d7a1e
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-08-31 07:15:15 +0300

    xx
    ss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import "./About.css";
 import about from "../../assets/Imgs/about.jpg";
 import AboutBox from "./AboutBox";
-import pdf from '../pricing/pdf/قائمة اسعار musta12.pdf'
+import pdf from "./pdf/Mustafa_Hassan.pdf";
 
 const About = () => {
   return (
-    <section className='about countainer section' id="about">
+    <section className='about countainer section' id='about'>
       <h2 className='section__title'>About Me</h2>
       <div className='about__countainer grid'>
-        <div className="left">
-           <img src={about} alt='' className='about__img' />
+        <div className='left'>
+          <img src={about} alt='' className='about__img' />
         </div>
-       
+
         <div className='about__data grid'>
           <div className='about__info'>
             <p className='about__description'>
-              I am Mustafa Hassan, Graphic Designer from Jeddah,Saudi Arabia. I have
-              rich experience in design and Photograph and video editing,
+              I am Mustafa Hassan, Graphic Designer from Jeddah,Saudi Arabia. I
+              have rich experience in design and Photograph and video editing,
               also I am good at Motion Graphic.
             </p>
             <a href={pdf} download={pdf} className='btn'>
@@ -53,10 +53,10 @@ const About = () => {
                 <span className='skills__percentage video-editing'></span>
               </div>
             </div>
-          </div>   
+          </div>
         </div>
       </div>
-      <AboutBox/>
+      <AboutBox />
     </section>
   );
 };
